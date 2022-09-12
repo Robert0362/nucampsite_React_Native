e1212ea24dd6212b7fa826651d8d0d193ca5558a
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-09-12 07:26:01 -0400

    LoginScreen

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -21,6 +21,8 @@ import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import FavoritesScreen from './FavoritesScreen';
+import LoginScreen from './LoginScreen';
+
 
 const Drawer = createDrawerNavigator();
 
@@ -132,6 +134,29 @@ const FavoritesNavigator = () => {
                     headerLeft: () => (
                         <Icon
                             name='heart'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    );
+};
+
+const LoginNavigator = () => {
+    const Stack = createStackNavigator();
+    return (
+        <Stack.Navigator screenOptions={screenOptions}>
+            <Stack.Screen
+                name='Login'
+                component={LoginScreen}
+                options={({ navigation }) => ({
+                    title: 'Favorite Campsites',
+                    headerLeft: () => (
+                        <Icon
+                            name='sign-in'
                             type='font-awesome'
                             iconStyle={styles.stackIcon}
                             onPress={() => navigation.toggleDrawer()}
@@ -269,6 +294,21 @@ const Main = () => {
                         drawerIcon: ({ color }) => (
                             <Icon
                                 name='heart'
+                                type='font-awesome'
+                                size={24}
+                                iconStyle={{ width: 24 }}
+                                color={color}
+                            />
+                        )
+                    }}
+                />
+                <Drawer.Screen
+                    name='Login'
+                    component={LoginNavigator}
+                    options={{
+                        drawerIcon: ({ color }) => (
+                            <Icon
+                                name='sign-in'
                                 type='font-awesome'
                                 size={24}
                                 iconStyle={{ width: 24 }}
