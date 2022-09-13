6d10e00f7a2482d1d1406195abd7fe2c403f4acf
Robert Schwindler <robertmichaelschwindler@gmail.com>
2022-09-13 07:16:24 -0400

    Social Sharing

--- features/campsites/RenderCampsite.js
+++ features/campsites/RenderCampsite.js
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View, PanResponder, Alert } from 'react-native';
+import { StyleSheet, Text, View, PanResponder, Alert, Share } from 'react-native';
 import { Card, Icon } from 'react-native-elements';
 import { baseUrl } from '../../shared/baseUrl';
 import * as Animatable from 'react-native-animatable'
 import { useRef }from 'react';
 
+
 const RenderCampsite = (props) => {
     const { campsite } = props;
+
+    const shareCampsite = (title, message, url) => {
+        Share.share(
+            {
+                title,
+                message: `${title}: ${message} ${url}`,
+                url  
+            },
+            {
+                dialogTitle: 'Share ' + title
+            }
+        );
+    }
 
     const view = useRef();
 
@@ -84,6 +98,20 @@ const RenderCampsite = (props) => {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name='share'
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => 
+                                shareCampsite(
+                                    campsite.name,
+                                    campsite.description,
+                                    baseUrl + campsite.image
+                                )
+                            }
                         />
                     </View>
                 </Card>
